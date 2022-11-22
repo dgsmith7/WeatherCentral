75fba5ae9284c2efd24cddb18c19b1cd85e59019
dgsmith7@hotmail.com <dgsmith7@hotmail.com>
2022-11-22 10:41:52 -0600

    fired up node app.  fingers crossed

---
+++ index.js
@@ -0,0 +1,8 @@
+let express = require('express');
+let app = express();
+app.get('/', function (req, res) {
+res.send('Weather Central Web Application is UP');
+});
+app.listen(8081, function () {
+    console.log('Weather Central Web Application running on port 8081!');
+});
