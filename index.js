9c96202361899442ca369652f2d2432921fbd50d
dgsmith7@hotmail.com <dgsmith7@hotmail.com>
2022-11-22 14:33:38 -0600

    still working on this

--- index.js
+++ index.js
@@ -2,15 +2,18 @@
 
 (function () {
     let express = require('express');
+    let path = require('path');
 
     let app = express();
 
     app.use(express.static('assets/js'));
 
     app.get('/', function (req, res) {
-        res.send('Weather Central Web Application is UP');
+        res.sendFile(path.join('/assets/js', '/index.html'));
+//        res.send('Weather Central Web Application is UP');
     });
     app.listen(80, function () {
         console.log('Weather Central Web Application running on port 80!');
     });
+
 }());
