db8cbd68c34ba0c614c53414c38148a9fd1bf707
dgsmith7@hotmail.com <dgsmith7@hotmail.com>
2022-11-22 16:37:05 -0600

    changed package.json

--- index.js
+++ index.js
@@ -12,7 +12,7 @@
         res.sendFile(path.join(__dirname,'/weather_map.html'));
 //        res.send('Weather Central Web Application is UP');
     });
-    app.listen(80, function () {
+    app.listen(8080, function () {
         console.log('Weather Central Web Application running on port 80!');
     });
 
