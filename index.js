1d7d2de49bb3c58b62fff5a417eaf1aab2dad522
dgsmith7@hotmail.com <dgsmith7@hotmail.com>
2022-11-22 11:39:46 -0600

    still working on this

--- index.js
+++ index.js
@@ -7,5 +7,5 @@ app.get('/', function (req, res) {
 res.send('Weather Central Web Application is UP');
 });
 app.listen(8081, function () {
-    console.log('Weather Central Web Application running on port 8081!' + `${_self.MY_NEW_ENV_VAR}`);
+    console.log('Weather Central Web Application running on port 8081!');
 });
